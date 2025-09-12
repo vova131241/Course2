abe001931a725ae64efd2106cf03b767f1582256
vova131241 <strashivskyivolodymyr@dspu.edu.ua>
2025-09-12 17:38:16 +0300

    Add files via upload

---
+++ lab_2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int CalculateXValue(int q, int r);
+int CalculateImp(int p, int q, int r);
+
+int main() {
+    cout << "p q r | q∨r | p -> (q v r)\n";
+    cout << "---------------------------\n";
+
+    for (int p = 0; p <= 1; p++) {
+        for (int q = 0; q <= 1; q++) {
+            for (int r = 0; r <= 1; r++) {
+                int q_or_r = CalculateXValue(q, r);
+                int f = CalculateImp(p, q, r);
+
+                cout << " " << p << " " << q << " " << r
+                    << "|  " << q_or_r
+                    << "  |      " << f << "\n";
+            }
+        }
+    }
+
+    cout << "\n=== Перевірка для власних значень ===\n";
+    int p, q, r;
+    cout << "Enter p (0/1): "; cin >> p;
+    cout << "Enter q (0/1): "; cin >> q;
+    cout << "Enter r (0/1): "; cin >> r;
+
+    int xValue = CalculateXValue(q, r);
+    int imp = CalculateImp(p, q, r);
+
+    cout << "\np q r | q v r | p -> (q v r)\n";
+    cout << "------------------------------\n";
+    cout << p << " " << q << " " << r
+        << " |  " << xValue
+        << "    |      " << imp << "\n";
+
+    return 0;
+}
+
+int CalculateXValue(int q, int r) {
+    return q || r;
+}
+
+int CalculateImp(int p, int q, int r) {
+    return (!p) || CalculateXValue(q, r);
+}
---
+++ lab_2test.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_2/lab_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab2test
+{
+	TEST_CLASS(lab2test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int p = 1;
+			int q = 0;
+			int r = 0;
+
+			int xValue = CalculateXValue(q, r);
+			int imp = CalculateImp(p, q, r);
+			int actual = 0;
+			Assert::AreEqual(imp, actual);
+		}
+	};
+}
